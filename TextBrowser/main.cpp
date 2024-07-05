4edf2553bdc77f54f3db7c05d7c55c87c164465d
Findof <joeargon21@gmail.com>
2024-07-05 14:28:58 -0400

    Search History

--- TextBrowser/main.cpp
+++ TextBrowser/main.cpp
@@ -10,16 +10,96 @@ std::string trim(const std::string& str) {
     return str.substr(first, last - first + 1);
 }
 
+void printOptions() {
+    std::cout << "What do you want to do?" << std::endl;
+    std::cout << "1: Go to a website" << std::endl;
+    std::cout << "2: Go to previous website in history" << std::endl;
+    std::cout << "3: Go to next website in history" << std::endl;
+    std::cout << "4: Create a new tab (not functional)" << std::endl;
+    std::cout << "5: Switch tabs (not functional)" << std::endl;
+    std::cout << "6: Go to current website" << std::endl;
+    std::cout << "-";
+}
+
+void clear() {
+    std::cout << "\033[2J\033[1;1H";
+}
+
 void render(std::string filename);
 
 int main() {
+    std::string history[50];
+    int historySize = 50;
+    int historyPos = 0;
+
     std::string filename;
+    int option;
+
     while (true) {
-        std::cout << "\033[2J\033[1;1H";
-        std::cout << "Enter a website:" << std::endl;
-        std::cin >> filename;
-        std::cout << "\033[2J\033[1;1H";
-        render(filename);
+
+        clear();
+
+        printOptions();
+        std::cin >> option;
+
+        clear();
+
+        switch (option) {
+        case 1:
+            std::cout << "Enter a website:" << std::endl;
+            std::cin >> filename;
+            clear();
+
+            if (historyPos >= historySize) {
+                historySize *= 2;
+                history->resize(historySize * sizeof(std::string));
+            }
+            
+            history[historyPos] = filename;
+            historyPos++;
+
+            for (int i = historyPos; i < historySize; i++) {
+                history[i] = "";
+            }
+
+            render(filename);
+            break;
+        case 2:
+            if (historyPos > 1) {
+                clear();
+
+                filename = history[historyPos - 2];
+                historyPos--;
+
+                render(filename);
+            }
+            break;
+        case 3:
+            if (history[historyPos] != "") {
+                clear();
+
+                filename = history[historyPos];
+                historyPos++;
+
+                render(filename);
+            }
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            if (historyPos > 0) {
+                clear();
+
+                filename = history[historyPos - 1];
+
+                render(filename);
+            }
+            break;
+        }
+
+        
     }
     return 0;
 }
@@ -69,7 +149,7 @@ void render(std::string filename) {
         }
     }
 
-    std::cout << "\n\nPress enter to search" << std::endl;
+    std::cout << "\n\nPress enter to continue" << std::endl;
     std::cin.get();
     std::cin.get();
 
